e41a54068deafe086c683ab708a0d82646419453
You Wu <youwuyou@student.ethz.ch>
2026-09-22 12:25:36 +0200

    [HW01] finished ex1-3

--- Homework/week1/Week1.lean
+++ Homework/week1/Week1.lean
@@ -22,8 +22,7 @@ Prove the following proposition in **term**-mode.
 -/
 theorem Q1 (p : P) (q : Q) (pr : P → R) (qrs : Q ∧ R → S) : S :=
   -- You are not allowed to use tactics (`by` keyword) in this task
-  sorry
-
+  qrs (And.intro q (pr p)) -- here we just curried toward the goal type
 /--
 Exercise 2:
 
@@ -31,7 +30,21 @@ Prove the following proposition.
 You may only use the tactics `constructor`, `intro`, `apply`, and `exact`.
 -/
 theorem Q2 : (P → Q ∧ R) ↔ ((P → Q) ∧ (P → R)) := by
-  sorry
+  constructor
+  · -- Subgoal 1: (P → Q ∧ R) → (P → Q) ∧ (P → R)
+    intro h
+    constructor
+    · intro hp
+      apply h at hp
+      exact hp.left
+    · intro hp
+      apply h at hp
+      exact hp.right
+  · -- Subgoal 2: (P → Q) ∧ (P → R) → P → Q ∧ R
+    intro lhs p
+    constructor
+    · exact lhs.left p
+    · exact lhs.right p
 
 /--
 Exercise 3:
@@ -41,7 +54,26 @@ Prove the following proposition. You may only use the tactics
 You may also introduce local definitions using `let`.
 -/
 theorem Q3 : ((P → Q) ∧ (P → ¬ Q)) ↔ ¬ P := by
-  sorry
+  constructor
+  · -- subgoal 1: (P → Q) ∧ (P → ¬Q) → ¬P
+    intro lhs              -- left-hand side
+    by_contra p            -- call it p : P
+    let h_pq := lhs.left
+    let q := h_pq p
+    let h_pnq := lhs.right
+    let nq := h_pnq p
+    contradiction
+  · -- subgoal 2: ¬P → (P → Q) ∧ (P → ¬Q)
+    intro np
+    constructor
+    · -- subsubgoal 2.1
+      intro p       -- with this we have both p : P, np : ¬P present
+      by_contra     -- switch the goal to `False` type
+      contradiction -- done :)
+    · -- subsubgoal 2.2
+      intro p       -- similarly..
+      by_contra
+      contradiction
 
 end Logic
 
