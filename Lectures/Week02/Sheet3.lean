569824a58ae2cc2ee3f6ec70d97b2b327a2642a9
You Wu <youwuyou@student.ethz.ch>
2026-09-23 18:15:32 +0200

    [week02] finished sheet 3 at home

--- Lectures/Week02/Sheet3.lean
+++ Lectures/Week02/Sheet3.lean
@@ -54,7 +54,14 @@ example : ∀ x ∈ A ∪ B, x ∈ A ∪ B ∪ C:= by
 #check mem_union
 #check subset_def
 
-lemma my_union_subset_imp :  A ⊆ C ∧ B ⊆ C → A ∪ B ⊆ C := by sorry
+lemma my_union_subset_imp :  A ⊆ C ∧ B ⊆ C → A ∪ B ⊆ C := by
+  intro ⟨h_AC, h_BC⟩
+  rw [subset_def]
+  intro x h_AB
+  rw [mem_union] at h_AB
+  cases h_AB with
+  | inl ha => exact h_AC ha
+  | inr hb => exact h_BC hb
 
 -- Extend my_union_subset_imp to my_union_subset_iff
 -- You are allowed to use *only* these two lemmas.
@@ -64,36 +71,57 @@ lemma my_union_subset_imp :  A ⊆ C ∧ B ⊆ C → A ∪ B ⊆ C := by sorry
 -- running example
 lemma my_union_subset_iff :  A ⊆ C ∧ B ⊆ C ↔ A ∪ B ⊆ C := by
   constructor
+  -- subgoal 1: A ⊆ C ∧ B ⊆ C → A ∪ B ⊆ C
   · intro a
     exact my_union_subset_imp A B C a
-  · intro
-    rw [subset_def] at a
+  -- subgoal 2: A ∪ B ⊆ C → A ⊆ C ∧ B ⊆ C
+  · intro lhs
     constructor
+    -- subsubgoal 2.1: A ⊆ C
     · rw [subset_def]
       intro x hx
-      apply a
-      rw [mem_union]
-      left
+      apply lhs
+      left -- union is same as OR, we take left part
       exact hx
-    · sorry
+    -- subsubgoal 2.1: B ⊆ C
+    · rw [subset_def]
+      intro x hx
+      apply lhs
+      rw [mem_union]
+      right
+      exact hx
 
 -- Exercise 4: you may want to use my_union_subset_iff
-example : B ⊆ A → C ⊆ A → B ∪ C ⊆ A := by sorry
+example : B ⊆ A → C ⊆ A → B ∪ C ⊆ A := by
+  intro h1 h2 x h
+  rw [mem_union] at h
+  cases h with
+  | inl hb => exact h1 hb
+  | inr hc => exact h2 hc
+
 
 /-! New tactics
- * `ext`  -- extensionality. Proving that two functions are identical. Since sets are functions in Lean, `ext` can be used to prove set equality.
+ * `ext`  -- extensionality. Proving that two functions are identical.
+  Since sets are functions in Lean,
+  `ext` can be used to prove set equality.
 -/
 
 -- example
 lemma inter_comm : A ∩ B = B ∩ A := by
   ext x
   constructor
+  -- subgoal 1: x ∈ A ∩ B → x ∈ B ∩ A
   · intro a
     rw [mem_inter_iff]
     rw [mem_inter_iff] at a
     obtain ⟨ha,hb⟩ := a
     exact ⟨hb,ha⟩
-  · sorry
+  -- subgoal 2: x ∈ B ∩ A → x ∈ A ∩ B
+  · intro lhs
+    rw [mem_inter_iff]
+    rw [mem_inter_iff] at lhs
+    obtain ⟨left, right⟩ := lhs
+    exact And.intro right left
 
 -- example
 lemma absorption_law : A ∩ (A ∪ B) = A := by
@@ -110,4 +138,18 @@ lemma absorption_law : A ∩ (A ∪ B) = A := by
       exact a
 
 -- Exercise 5
-lemma union_comm : A ∪ B = B ∪ A := by sorry
+lemma union_comm : A ∪ B = B ∪ A := by
+  ext x
+  rw [union_def]
+  rw [union_def]
+  constructor
+  -- subgoal 1: x ∈ A ∪ B → x ∈ B ∪ A
+  · intro lhs
+    cases lhs with
+    | inl ha => right; exact ha
+    | inr hb => left; exact hb
+  -- subgoal 2: x ∈ B ∪ A → x ∈ A ∪ B
+  · intro lhs
+    cases lhs with
+    | inl hb => right; exact hb
+    | inr ha => left; exact ha
